30c303fc10f6a5835970644cb23ffa40e7ba2738
henrique221 <hborges9294@gmail.com>
2018-04-30 16:12:32 -0300

    verifica mes e getMes

--- data.php
+++ data.php
@@ -8,6 +8,7 @@ class Data
     public function __construct($dia, $mes, $ano)
     {
         $this->verificaDia($dia);
+        $this->verificaMes($mes);
     }
     public function verificaDia($dia)
     {
@@ -33,7 +34,32 @@ class Data
     {
         return $this->dia;
     }
+
+    public function verificaMes($mes)
+    {
+        if ($mes <= -1) {
+            $mes = $mes * -1;
+        }
+        if ($mes == 0){
+            return "Mês invalido";
+        }
+
+        switch ($mes) {
+            case ($mes > 12):
+                return "Mes inexistente";
+            case ($mes < 10):
+                $this->mes = sprintf("0%u",(string)$mes);
+                return $this->mes;
+            case ($mes >= 10):
+                $this->mes = (string)$mes;
+                break;
+        }
+    }
+    public function getMes()
+    {
+        return $this->mes;
+    }
 }
 
-$data = new Data(25, 11, 2018);
-var_dump($data->dia, $data->mes, $data->ano);
+$data = new Data(25, 5, 2018);
+var_dump($data->getDia(),$data->getMes(), $data->ano);
