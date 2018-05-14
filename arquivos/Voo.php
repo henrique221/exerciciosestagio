6bece64cc9771ced7cf51d6b85890102459fd023
henrique221 <hborges9294@gmail.com>
2018-05-14 13:56:16 -0300

    datas em formato string

--- arquivos/Voo.php
+++ arquivos/Voo.php
@@ -4,16 +4,19 @@ class Voo
 {
     public $numeroVoo;
     public $data;
+    public $vagas = 100;
+
     public function __construct()
     {
         $data = $this->data;
+        $vagas = $this->vagas;
         $numeroVoo = $this->numeroVoo;
     }
 
     public function setDataEHorario($d, $m, $Y, $H, $i, $s)
     {
         $format = "d/m/Y H:i:s";
-        $dataVoo = DateTime::createFromFormat($format, '11/05/1992 11:50:00');
+        $dataVoo = DateTime::createFromFormat($format, sprintf("$d/$m/$Y $H:$i:$s"));
         $this->data = $dataVoo->format($format);
 
         return $this->data;
@@ -21,7 +24,7 @@ class Voo
 }
 
 $voo = new Voo;
-$voo->setDataEHorario(11, 05, 2019, 15, 20, 00);
+$voo->setDataEHorario('11', '05', '2019', '15', '20', '00');
 
 var_dump(
     $voo
--- testes/VooTest.php
+++ testes/VooTest.php
@@ -2,11 +2,17 @@
 
 use PHPUnit\Framework\TestCase as PHPUnit;
 
-require_once './arquivos/Data.php';
+require_once './arquivos/Voo.php';
 
 class DataTest extends PHPUnit
 {
     /**
      * @test
      */
+    public function deveRetornarADataEHorarioDoVoo()
+    {
+        $voo1 = new Voo;
+        $voo1->setDataEHorario(10, 10, 2018, 10, 10, 10);
+        $this->assertEquals("10/10/2018 10:10:10", $voo1->data);
+    }
 }
