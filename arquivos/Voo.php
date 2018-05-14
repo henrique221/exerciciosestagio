8f6d137bcbdd51df0da32b52cada1188c96d7f58
henrique221 <hborges9294@gmail.com>
2018-05-14 11:44:36 -0300

    Classe voo

--- arquivos/Voo.php
+++ arquivos/Voo.php
@@ -1,3 +1,28 @@
 <?php
-$today = date("d/m/Y", mktime(0,0,0,7,1,2000)); 
-var_dump($today);
+
+class Voo
+{
+    public $numeroVoo;
+    public $data;
+    public function __construct()
+    {
+        $data = $this->data;
+        $numeroVoo = $this->numeroVoo;
+    }
+
+    public function setDataEHorario($d, $m, $Y, $H, $i, $s)
+    {
+        $format = "d/m/Y H:i:s";
+        $dataVoo = DateTime::createFromFormat($format, '11/05/1992 11:50:00');
+        $this->data = $dataVoo->format($format);
+
+        return $this->data;
+    }
+}
+
+$voo = new Voo;
+$voo->setDataEHorario(11, 05, 2019, 15, 20, 00);
+
+var_dump(
+    $voo
+);
---
+++ testes/VooTest.php
@@ -0,0 +1,12 @@
+<?php
+
+use PHPUnit\Framework\TestCase as PHPUnit;
+
+require_once './arquivos/Data.php';
+
+class DataTest extends PHPUnit
+{
+    /**
+     * @test
+     */
+}
